507581e0a6a5d999b569bd95b0ff06c399e5bfb8
Barotbek Turxonov <109985351+BarotbekTurxonov@users.noreply.github.com>
2023-02-10 01:02:19 +0500

    Added Data Folder

---
+++ data/config.py
@@ -0,0 +1,10 @@
+from environs import Env
+
+# environs kutubxonasidan foydalanish
+env = Env()
+env.read_env()
+
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = "5880532227:AAECSufh-b3O1haT5k62-CDdqTsdXvxflT4"  # Bot token
+ADMINS = [5235865310]
+    
